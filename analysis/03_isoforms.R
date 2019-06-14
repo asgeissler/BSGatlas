e4f2ca1e7a543060ca43b11af05711e4cabb369b
Adrian Sven Geissler <adrian@rth.dk>
2019-06-14 18:17:31 +0200

    compute operon hierarchy

--- analysis/03_isoforms.R
+++ analysis/03_isoforms.R
@@ -236,7 +236,7 @@ short.span %>%
 #####################################
 # 4. Infer operons/isoforms
 
-library(tidygraph)
+library(tidygrap)
 
 edges <- transcripts %>%
   select(from = id, to = genes) %>%
@@ -286,4 +286,93 @@ save(operons, file = 'analysis/03_operons.rda')
 # 5. Make statistics and nice plots
 
 
+load('analysis/03_operons.rda')
+
+library(venn)
+genes <- merging$merged_genes %>%
+  rename(id = merged_id)
+
+# > nrow(operons$operon)
+# [1] 1146
+# > nrow(operons$transcrip)
+# [1] 2476
+
+
+  
+pdf(file = 'analysis/03_venn_trans.pdf')
+operons$transcrip %>%
+  select(id, src) %>%
+  separate_rows(src, sep = ';') %>%
+  group_by(src) %>%
+  do(i = list(.$id)) %>%
+  with(set_names(map(i, 1), src)) %>%
+  venn(cexil = 1.3,
+       cexsn = 1.5, zcolor = 'style')
+dev.off()
+
+
+
 # ratio of genes not described by any operon
+operons$transcrip %>%
+  select(src, genes) %>%
+  separate_rows(genes, sep = ';') %>%
+  separate_rows(src, sep = ';') %>%
+  mutate(has = TRUE) %>%
+  select(id = genes, src, has) %>%
+  unique %>%
+  spread(src, has) -> has.mat
+
+# STATISTICS 
+# ratio of genes with a transcript
+select(genes, id) %>%
+  left_join(has.mat) %>%
+  mutate_all(replace_na, FALSE) %>%
+  mutate(BSGatlas = BsubCyc | DBTBS | SubtiWiki) %>%
+  select(- id) %>%
+  map(sum) %>%
+  map(divide_by, nrow(genes)) %>%
+  map(multiply_by, 100) %>%
+  tibble(`% genes with transcript` = ., src = names(.)) %>%
+  unnest %>%
+  # Total number of transcripts
+  left_join(
+    operons$transcrip %>%
+      select(id, src) %>%
+      separate_rows(src, sep = ';') %>%
+      unique %>%
+      count(src) %>%
+      spread(src, n) %>%
+      mutate(BSGatlas =  nrow(operons$transcrip) ) %>%
+      gather('src', 'number of transcripts'),
+    'src'
+  ) %>%
+  # Total number of operons
+  left_join(
+    operons$operon %>%
+      select(op = id, id = isoforms) %>%
+      separate_rows(id, sep = ';') %>%
+      left_join(operons$transcrip, 'id') %>%
+      select(op, src) %>%
+      separate_rows(src, sep = ';') %>%
+      unique %>%
+      count(src) %>%
+      spread(src, n) %>%
+      mutate(BSGatlas = nrow(operons$operon)) %>%
+      gather('src', 'number of operons')
+  ) %>%    
+  gather('what', 'value', -src) %>%
+  mutate(src = fct_relevel(src, 'DBTBS', 'BsubCyc', 'SubtiWiki', 'BSGatlas')) %>%
+  ggplot(aes(x = '', y = value, fill = src)) +
+  ggsci::scale_fill_jama(name = 'resource') +
+  geom_bar(stat = 'identity', position = 'dodge') +
+  geom_text(aes(label=value %>% round()),
+            position=position_dodge(width=0.9), vjust=-0.25) +
+  xlab(NULL) +
+  ylab(NULL) +
+  facet_wrap(~ what, scales = 'free') +
+  theme_minimal(base_size = 14) +
+  theme(strip.text = element_text(size = 14))
+
+ggsave(file = 'analysis/03_bars.pdf',
+       width = 25, height = 9, units = 'cm')
+
