9e624069aaab1319abbd1f85de0d4b36d33a4535
Adrian Sven Geissler <adrian@rth.dk>
2019-07-04 10:34:28 +0200

    Tu investigation

--- analysis/07_isoforms.R
+++ analysis/07_isoforms.R
@@ -8,6 +8,7 @@
 # between nodes with only outgoing and only ingoing edges
 
 source('scripts/graph_paths.R')
+source('scripts/frame_helpers.R')
 source('analysis/00_load.R')
 
 load('analysis/02_merging.rda')
@@ -87,20 +88,22 @@ bind_rows(
   select(number, name = id, type) -> nodes
 
 
-edges <- bind_rows(
+edges.full <- bind_rows(
   tu.edges,
   utr.edges,
   tssterm.edges,
   empty.edges
 ) %>%
+  unique 
   # filter(!complete.cases(.))
+edges.full %>%
   mutate(row = 1:n()) %>%
   gather('key', 'name', from, to) %>%
   left_join(nodes, 'name') %>%
   select(row, key, number) %>%
   spread(key, number) %>%
   select(from, to) %>%
-  drop_na
+  drop_na -> edges
 
 iso.graph <- tbl_graph(nodes, edges, directed = TRUE)
 
@@ -126,4 +129,94 @@ paths %>%
     TSS, `5'UTR`, `a gene`, internal_UTR,
     `3'UTR`, `terminator`, count = n
   ) %>%
-  View
+  arrange(desc(count)) -> paths.stat
+
+assertthat::assert_that(
+  with(paths.stat, all(TSS <= 1)),
+  with(paths.stat, all(terminator <= 1)),
+  with(paths.stat, all(`5'UTR` <= 1)),
+  with(paths.stat, all(`3'UTR` <= 1)),
+  with(paths.stat, all(internal_UTR < `a gene`)),
+  with(paths.stat, all(`a gene` >= 1))
+)
+
+View(paths.stat)
+
+###############################################################################
+# Which paths have which TU?
+# How are the operons?
+# How many genes have still no transcript
+# What are the spans?
+# How many are due to the UTRs
+# How many paths have the same genes, but just differen 5/3 UTRs
+
+
+# > nrow(paths)
+# [1] 28133
+
+paths %>%
+# Which paths have which TU?
+  select(path, merged_id = value) %>%
+  semi_join(merging$merged_genes, 'merged_id') %>%
+  group_by(path) %>%
+  summarize(genes = merged_id %>%
+              sort %>%
+              clean_paste) -> genes.sorted
+
+genes.sorted %>%
+  left_join(tus, 'genes') %>%
+  select(path, tu = id, src) -> path.tu
+
+# check each path has only one TU
+assertthat::are_equal(
+  path.tu %>%
+    count(path) %>%
+    count(n) %>%
+    filter(n > 1) %>%
+    nrow,
+  0
+)
+
+path.tu %>%
+  drop_na %>%
+  count(tu) %>%
+  count(n) %>%
+  rename('paths per tu' = n, count = nn)
+# `paths per tu` count
+# 1  1385
+# 2   647
+# 3    85
+# 4   105
+# 5     1
+# 6    27
+# 8     2
+# 9     1
+
+path.tu %>%
+  drop_na %>%
+  select(tu) %>%
+  unique %>%
+  nrow
+# 2253 number of TUs used? is less?
+
+# Why are these tus not used?
+tus %>%
+  anti_join(path.tu, c('id' = 'tu')) %>%
+  select(id, src, genes) %>%
+  # nrow
+  # 221
+  head
+
+# interest <- c('BSGatlas-gene-21', 'BSGatlas-gene-23')
+# edges.full %>%
+#   filter((from %in% interest) | (to %in% interest)) %>%
+#   View
+# okay, should have been expected, because some TUs don't have both explicit 
+# TSS/terminator
+# Revist computation with placeholder for those ->
+
+# How are the operons?
+# How many genes have still no transcript
+# What are the spans?
+# How many are due to the UTRs
+# How many paths have the same genes, but just differen 5/3 UTRs
