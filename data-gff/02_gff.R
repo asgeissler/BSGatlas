bdd33f8694727be3a793e1bef9f839fd3d9d7e41
Adrian Sven Geissler <adrian@rth.dk>
2020-11-13 10:20:41 +0100

    Simplified GFF and .2f energy formatting

--- data-gff/02_gff.R
+++ data-gff/02_gff.R
@@ -215,7 +215,7 @@ bsg.boundaries$terminator %>%
     ID = id, start, end, strand,
     type = 'terminator',
     comment = sprintf(
-      'Based on: %s, Energy: %s[kcal/mol]',
+      'Based on: %s, Energy: %.2f [kcal/mol]',
       str_replace_all(src, ';', ','),
       energy
     )
@@ -254,82 +254,97 @@ bind_rows(
   arrange(start, desc(end)) %>%
   mutate_at(c('comment', 'synonyms', 'ec', 'go', 'subtiwiki_category',
               'kegg_pathways'),
-            ~ ifelse(is.na(.x), NA, sprintf('"%s"', .x))) -> dat
+            ~ ifelse(is.na(.x), NA, sprintf('"%s"', .x))) -> dat.full
 
-lines <- bind_rows(
-  tibble(line = c(
-    '##gff-version 3',
-    '#!genome-build ASM904v1',
-    '#!genome-build-accession NCBI_Assembly:GCF_000009045.1',
-    '##sequence-region NC_000964.3 1 4215606',
-    '##species https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=224308',
-    '#',
-    '# BSGatlas version 1',
-    '# A fine grained atlas of the Bacillus Subtillis genome',
-    '#',
-    '# This is a gff file representation of the BSGatlas for Bacillus subtilis',
-    '# according to the NCBI assembly ASM904v1. The columns are:',
-    '#',
-    '# 1. Chromosome: Here `ncbi168` for coherence with the browser hub',
-    '# 2. Source: Is BSGatlas for this annotation.',
-    '# 3. Type: The type for an annotation feature',
-    '# 4. Start: Start and ...',
-    '# 5. End: ... ending position of a feature.',
-    '# 6. Score: Here not used, constant dot as value',
-    '# 7. Strand: Strand of a feature. Possible values are {"+", "-", "."}',
-    '# 8. Phase: Translation offset phase for coding sequences. Is either "0" or ".".',
-    '# 9. List of attributes',
-    '# ',
-    '# Attibutes are a semicolon separated list of <key>=<value> pairs.',
-    '# Alternative values are a comma separated list.',
-    '# Example: Name=GeneA;Synonyms=GeneX,GeneY',
-    '# ',
-    '# The following keys are used:',
-    '# ',
-    '# ID:                  Unique identifier for each row',
-    '# Parent:              List of hierarchical parent relationships',
-    '# Derives_from:        Link to gene from which a transcript feature originates',
-    '# Name:                The name of a feature',
-    '# locus_tag:           A locus identifier for genes; this often is the used identifier in external databases.',
-    '# Description:               Free descriptive text of a feature',
-    '# comment:              more ellaborate descriptive text of a feature',
-    '# synonyms:            List of alternative names',
-    '# go:                  List of gene function terms accodting to the Gene Ontology',
-    '# ec:                  List of Enzyme Classifications',
-    '# subtiwiki_category:  A gene classification system from SubtiWiki'
-  )),
-  dat %>% rowwise() %>% do(line = {
-    row <- .
-    row <- row %>%
-      as.list() %>%
-      discard(is.na) %>%
-      unlist()
-    
-    # entries without an explicit column
-    rest <- row %>%
-      discard(names(.) %in% c('type', 'start', 'end', 'strand'))
-    attributes <- paste(
-      names(rest), rest, sep = '='
-    ) %>%
-      clean_paste(sep = ';')
-    
-    row <- as.list(row)
-    paste(
-      'basu168',
-      'BSGatlas',
-      row$type,
-      row$start,
-      row$end,
-      '.',
-      row$strand,
-      ifelse(row$type == 'CDS', '0', '.'),
-      attributes,
-      sep = '\t'
+# (optional)
+dat.full %>%
+  filter(!(type %in% c('exon', 'TSS', 'terminator', 'transcript'))) %>%
+  select(- transcribed_from) %>%
+  mutate_at('Parent', str_remove_all, 'BSGatlas-transcript-[0-9]*') %>%
+  mutate_at('Parent', str_replace_all, ',,*', ',') %>%
+  mutate_at('Parent', str_remove_all, '^,$') %>%
+  mutate_at('Parent', ~ ifelse(.x == '', NA_character_, .x)) -> dat.simple
+
+list(
+  'data-gff/BSGatlas _v1.1.gff' = dat.full,
+  'data-gff/BSGatlas-without_transcripts_v1.1.gff' = dat.simple
+) %>%
+  map2(names(.), function(dat, path){
+    lines <- bind_rows(
+      tibble(line = c(
+        '##gff-version 3',
+        '#!genome-build ASM904v1',
+        '#!genome-build-accession NCBI_Assembly:GCF_000009045.1',
+        '##sequence-region NC_000964.3 1 4215606',
+        '##species https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=224308',
+        '#',
+        '# BSGatlas version 1.1',
+        '# A fine grained atlas of the Bacillus Subtillis genome',
+        '#',
+        '# This is a gff file representation of the BSGatlas for Bacillus subtilis',
+        '# according to the NCBI assembly ASM904v1. The columns are:',
+        '#',
+        '# 1. Chromosome: Here `basu168` for coherence with the browser hub',
+        '# 2. Source: Is BSGatlas for this annotation.',
+        '# 3. Type: The type for an annotation feature',
+        '# 4. Start: Start and ...',
+        '# 5. End: ... ending position of a feature.',
+        '# 6. Score: Here not used, constant dot as value',
+        '# 7. Strand: Strand of a feature. Possible values are {"+", "-", "."}',
+        '# 8. Phase: Translation offset phase for coding sequences. Is either "0" or ".".',
+        '# 9. List of attributes',
+        '# ',
+        '# Attibutes are a semicolon separated list of <key>=<value> pairs.',
+        '# Alternative values are a comma separated list.',
+        '# Example: Name=GeneA;Synonyms=GeneX,GeneY',
+        '# ',
+        '# The following keys are used:',
+        '# ',
+        '# ID:                  Unique identifier for each row',
+        '# Parent:              List of hierarchical parent relationships',
+        '# Derives_from:        Link to gene from which a transcript feature originates',
+        '# Name:                The name of a feature',
+        '# locus_tag:           A locus identifier for genes; this often is the used identifier in external databases.',
+        '# Description:         Free descriptive text of a feature',
+        '# comment:             more ellaborate descriptive text of a feature',
+        '# synonyms:            List of alternative names',
+        '# go:                  List of gene function terms accodting to the Gene Ontology',
+        '# ec:                  List of Enzyme Classifications',
+        '# subtiwiki_category:  A gene classification system from SubtiWiki',
+        '# kegg_pathways:       Information about potentially associated KEGG pathways'
+      )),
+      dat %>% rowwise() %>% do(line = {
+        row <- .
+        row <- row %>%
+          as.list() %>%
+          discard(is.na) %>%
+          unlist()
+        
+        # entries without an explicit column
+        rest <- row %>%
+          discard(names(.) %in% c('type', 'start', 'end', 'strand'))
+        attributes <- paste(
+          names(rest), rest, sep = '='
+        ) %>%
+          clean_paste(sep = ';')
+        
+        row <- as.list(row)
+        paste(
+          'basu168',
+          'BSGatlas',
+          row$type,
+          row$start,
+          row$end,
+          '.',
+          row$strand,
+          ifelse(row$type == 'CDS', '0', '.'),
+          attributes,
+          sep = '\t'
+        )
+      }) %>%
+        unnest
     )
-  }) %>%
-    unnest
-)
-
-pull(lines, line) %>%
-  write_lines('data-gff/BSGatlas_v1.1.gff')
+    
+    pull(lines, line) %>% write_lines(path)
+})
  
